dd1df13863ed0ee106c05abe0111b576ebe9e641
git <65435748@qq.com>
2019-01-15 11:57:31 +0800

    fisrt init

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+
+    printf("进入存钱取钱管理系统\n");
+    while (1)
+    {
+        int a;
+
+        printf("1---存钱\n");
+        printf("2---取钱\n");
+        printf("3---余额\n");
+        printf("4---退出\n");
+
+        printf("请输入功能编号\n");
+        scanf("%d",&a);
+    
+        if(a==1){
+
+        }
+        if(a==2){}
+        if(a==4){}
+        if(a==5){}
+    }
+    return 0;
+}
